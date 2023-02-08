0d906d1ce27da74eed77d9af430d584118687565
nikolajsvv <nikolajsvv@gmail.com>
2023-02-07 22:48:20 -0500

    update to index.js

--- functions/storeCurrentlyPlaying.js
+++ functions/storeCurrentlyPlaying.js
@@ -10,18 +10,16 @@ const storeCurrentlyPlaying = () => {
       data += chunk;
     });
     res.on("end", () => {
-      if (data === "") {
-        console.log("Nothing playing...");
-        return;
-      }
       const $ = cheerio.load(data);
       const body = $("pre").text();
       const bodyObject = JSON.parse(body);
-      const songName = bodyObject.item.name;
-      const artist = bodyObject.item.artists[0].name;
-      const songInfo = `${songName} by ${artist}`;
+      if (bodyObject) {
+        const songName = bodyObject.item.name;
+        const artist = bodyObject.item.artists[0].name;
+        const songInfo = `${songName} by ${artist}`;
 
-      fs.writeFileSync(path.join(__dirname, "../data/song.txt"), songInfo);
+        fs.writeFileSync(path.join(__dirname, "../data/song.txt"), songInfo);
+      }
     });
   });
 };
