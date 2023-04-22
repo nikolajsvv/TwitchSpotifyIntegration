2d0ae67b518e4e07db70e8e061f87d20b3f6fb6e
nikolajsvv <nikolajsvv@gmail.com>
2023-04-22 10:05:21 -0400

    rename server files & add single executable main file

--- launchTwitchAPI.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\Nikolajs\Desktop\OSTherapy\Twitch\ChatBot\twitch-chat-spotify-bot
-npm start
--- start.bat
+++
@@ -1,2 +0,0 @@
-start cmd /k "cd /d C:\Users\Nikolajs\Desktop\Code\ChatBot\twitch-chat-spotify-bot && node server.js"
-start cmd /k "cd /d C:\Users\Nikolajs\Desktop\Code\ChatBot\twitch-chat-spotify-bot && node index.js"
